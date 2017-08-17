41ccfb142110ec79f786628e4d00d75581dc24a1
Matt Kelsey <mattkelseymk@gmail.com>
2017-08-16 21:16:33 -0700

    add pid

--- src/main/java/com/example/ControlLoopDevelopment.java
+++ src/main/java/com/example/ControlLoopDevelopment.java
@@ -1,9 +1,5 @@
 package com.example;
 
-import java.awt.GridLayout;
-
-import javax.swing.JFrame;
-
 public class ControlLoopDevelopment {
     static MouseJFrame frame;
     public static void  main(String[] args) {
@@ -12,9 +8,34 @@ public class ControlLoopDevelopment {
 
     public static float timeOut = 0;
 
+     static float proportional = 0;
+     static float integral = 0;
+     static float derivative = 0;
+
+    static float lastP = 0;
+
+    static float output =  0;
+
+    static float kP = 3;
+    static float kI = 1;
+    static float kD = 4;
+
+    static long lastTime = System.currentTimeMillis();
+
     public static float calculations(RobotGraphic robot) {
-        System.out.println(robot.getDegreeRotation());
-        return -robot.getDegreeRotation();
+
+        long now = System.currentTimeMillis();
+        System.out.println(now);
+        //System.out.println(robot.getDegreeRotation());
+        proportional = -robot.getDegreeRotation();
+        integral += (proportional * (float)(now - lastTime));
+        System.out.println("integral: " + integral);
+        derivative = (proportional-lastP)/(float) (now - lastTime);
+      //  System.out.println(proportional + " " + integral + " " + derivative);
+        output = /*(integral * kI) + */(proportional * kP) + (derivative * kD);
+        lastTime = now;
+        lastP = proportional;
+        return output;
     }
 
 }
