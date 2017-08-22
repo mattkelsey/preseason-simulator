5d81e885222654eeb5b027582a06ae886e7913f3
Matt Kelsey <mattkelseymk@gmail.com>
2017-08-22 14:56:23 -0700

    allow multiple runs

--- src/main/java/com/example/ControlLoopDevelopment.java
+++ src/main/java/com/example/ControlLoopDevelopment.java
@@ -8,14 +8,17 @@ public class ControlLoopDevelopment {
         // setSpeed(speed_value) sets the robots speed of rotation
         // Put Thread.sleep(16); in all of your while loops or you're computer will explode.
         // The imu class allows access to imu readings.
-
+        
     }
 
     //<editor-fold desc="behind the scenes magic">
     static MouseJFrame frame;
     public static void  main(String[] args) throws InterruptedException {
         frame = new MouseJFrame("Robot Sim");
-        calculations();
+        while(true) {
+            calculations();
+            Thread.sleep(100);
+        }
     }
     private static void setSpeed(float d) {
         if (!frame.isDragging) {
